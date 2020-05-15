597d7127cc0666d1ee8e3ac748903cc10f37c620
Philip Top <phlptp@gmail.com>
2020-05-15 06:13:36 -0700

    use PROJECT_SOURCE_DIR instead of CMAKE_SOURCE_DIR and fix 32 bit windows build LAPACK_DIR guess

--- checkGetSuiteSparse.cmake
+++ checkGetSuiteSparse.cmake
@@ -11,17 +11,18 @@
 ##
 ## Created by jesnault (jerome.esnault@inria.fr) 2014-01-21
 ## Updated by jesnault (jerome.esnault@inria.fr) 2014-01-21
+## Updated by phlptp (top1@llnl.gov) 2020-05-15
 
 ## use standard cmake tar command
 macro(SuiteSparse_unzip whichZipFile)
 	get_filename_component(zipFile ${whichZipFile} NAME)
 	message(STATUS "unzip: please, wait until tar xzf ${whichZipFile} finished...")
 	execute_process( 	COMMAND ${CMAKE_COMMAND} -E tar xzf ${zipFile}
-						WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}	TIMEOUT 120
+						WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}	TIMEOUT 120
 						RESULT_VARIABLE resVar OUTPUT_VARIABLE outVar ERROR_VARIABLE errVar
 					)
 	if(${resVar} MATCHES "0")
-		message(STATUS "SuiteSparse just unziped in the source dir") ## OK
+		message(STATUS "SuiteSparse just unzipped in the source dir") ## OK
 		set(SUITESPARSE_DL_LAST OFF CACHE BOOL "get the last version of suiteSparse" FORCE)
 	else()
 		message(WARNING "something wrong with cmake tar command, redo or try to unzip by yourself...")
@@ -31,7 +32,7 @@ macro(SuiteSparse_unzip whichZipFile)
 endmacro()
 
 ## Try to auto download and/or auto unzip the Win3rdParty
-if(NOT EXISTS "${CMAKE_SOURCE_DIR}/SuiteSparse/CHOLMOD/Include/cholmod.h" AND NOT SUITESPARSE_DL_LAST)
+if(NOT EXISTS "${PROJECT_SOURCE_DIR}/SuiteSparse/CHOLMOD/Include/cholmod.h" AND NOT SUITESPARSE_DL_LAST)
 	message(STATUS "Apparently, SuiteSparse project is not yet download and extract here. Let me try to do it for you...")
 	set(SUITESPARSE_DL_LAST ON CACHE BOOL "get the last version of suiteSparse" FORCE)
 else()
@@ -41,7 +42,7 @@ endif()
 if(SUITESPARSE_DL_LAST)
 	# JL says: It seems the "current" symlink in the server is no longer available... we'll have to update this manually :-(
 	set(SUITESPARSE_URL "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.5.6.tar.gz")
-	set(SUITESPARSE_ZIP "${CMAKE_SOURCE_DIR}/SuiteSparse.tar.gz")
+	set(SUITESPARSE_ZIP "${PROJECT_SOURCE_DIR}/SuiteSparse.tar.gz")
 	if(SUITESPARSE_URL AND SUITESPARSE_ZIP)
 	
 		message(STATUS "try downloading...\n  src='${SUITESPARSE_URL}'\n  dst='${SUITESPARSE_ZIP}'\n  inactivity_timeout='15'")
